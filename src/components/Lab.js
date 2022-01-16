4fc656725d6cea7a02eb78d67f53e8277b3a37ec
Samuel <romerotitosamuel@gmail.com>
2022-01-16 02:16:44 -0400

    p

--- src/components/Lab.js
+++ src/components/Lab.js
@@ -1,19 +1,144 @@
-const Lab = () => {
-    /*  import { doc, setDoc } from "firebase/firestore";
-        const cityRef = doc(db, 'cities', 'BJ');
-        setDoc(cityRef, { capital: true }, { merge: true }); */
+import React, { useState, useEffect } from 'react'
+import firebaseApp from '../credentials'
+import { getFirestore, doc, setDoc, collection, getDocs, orderBy, query, deleteDoc, getDoc } from 'firebase/firestore'
+import { downHalfTone, divisor } from './functions'
+const db = getFirestore(firebaseApp)
 
-        
+const Lab = () => {
+    
+    const [songs, setSongs] = useState([])
+    const [visualData, setVisualData] = useState({})
+    const [edition, setEdition] = useState(false)
+    const [idToUpdate, setIdToUpdate] = useState('')
+
+    const getSongs = async () => {
+        const songsSnap = await getDocs(query(collection(db, "canciones"), orderBy("artista")))
+
+        songsSnap.forEach((ss) => {
+            let songData = Object.assign(ss.data(), { id: ss.id })
+            setSongs(songs => [...songs, songData])
+        })
+    }
+
+    const borrarDoc = async (id, titulo, artista) => {
+        let confirmacion = window.confirm(`Esta seguro que desea borar la canción ${titulo} de ${artista}`)
+
+        if (confirmacion) {
+            await deleteDoc(doc(db, "canciones", id));
+        }
+        window.location.reload()
+    }
+    const printVisual = async (visualId) => {
+        let visualDataSnap = await getDoc(doc(db, "canciones", visualId))
+        let pushed = Object.assign(visualDataSnap.data(), { id: visualId })
+        setVisualData(pushed)
+    }
+    const handlerSubmit = async (e) => {
+        e.preventDefault()
+
+        let titulo = e.target.titulo.value
+        let artista = e.target.artista.value
+        let bpm = e.target.bpm.value
+        let url = e.target.url.value
+        let letra = e.target.letra.value
+        let acordes = e.target.acordes.value
+
+        const objFromForm = { titulo, artista, bpm, url, letra, acordes }
+
+        if (edition) {
+            await setDoc(doc(db, 'canciones', idToUpdate), objFromForm)
+            printVisual(idToUpdate)
+            document.getElementById("formulario").reset()
+            setEdition(false)
+        } else {
+            await setDoc(doc(collection(db, "canciones")), objFromForm)
+            document.getElementById("formulario").reset()
+            alert(`Tu canción de ${artista} ha sido creada`)
+            window.location.reload()
+        }
+    }
+
+    const modoEditar = async (id) => {
+        let songEditSnap = await getDoc(doc(db, "canciones", id))
+        let wr = songEditSnap.data()
+
+        let titulo = document.getElementById('titulo'); titulo.value = wr.titulo
+        let artista = document.getElementById('artista'); artista.value = wr.artista
+        let bpm = document.getElementById('bpm'); bpm.value = wr.bpm
+        let url = document.getElementById('url'); url.value = wr.url
+        let letra = document.getElementById('letra'); letra.value = wr.letra
+        let acordes = document.getElementById('acordes'); acordes.value = wr.acordes
+
+        setEdition(true)
+        printVisual(id)
+        setIdToUpdate(id)
+    }
+    const ejecutarBajar = () => {
+        let contenido = document.getElementById('vis')
+        let nuevo = downHalfTone(contenido.textContent)
+        contenido.textContent = nuevo
+    }
+    const ejecutar = () => {
+        let contenido = document.getElementById('vis')
+        let nuevo = divisor(contenido.textContent)
+        contenido.textContent = nuevo
+
+     }
+
+    useEffect(() => getSongs(), [])
+
     return (<>
-        <div className = 'labBody'>
+        <div className='labBody'>
+
             <div className='labList'>
-                <h3>La lista</h3>
+                <h3>Listado</h3>
+                {songs.map((s) => {
+                    return (<>
+                        <div className='listFlex' key={s.id.toString()}>
+                            <div className='iconBlock'>
+                                <i className="material-icons" onClick={() => printVisual(s.id)}>preview</i>
+                                <i className="material-icons" onClick={() => modoEditar(s.id)}>edit</i>
+                                <i className="material-icons" onClick={() => borrarDoc(s.id, s.titulo, s.artista)}>delete_outline</i>
+                            </div>
+                            <div >
+                                {s.titulo} <em><small>- {s.artista}</small></em>
+                            </div>
+                        </div>
+                    </>)
+                })}
             </div>
-            <div className='labEdit'>
-                <h3>la edicion</h3>
-            </div>
+
             <div className='labShow'>
-                <h3>la vista previa</h3>
+                <h3>Vista previa   <i className="material-icons" onClick={() => modoEditar(visualData.id)}>edit</i></h3>    
+                <div className='prevEncabezado'>
+                    {visualData.titulo} - <small>{visualData.artista}</small>
+                </div>
+                <div className='prevContenido'>
+                    <pre>{visualData.letra}</pre><br /><hr /><br />
+                    <pre id='vis'>{visualData.acordes}</pre>
+
+                </div>
+            </div>
+
+            <div className='labEdit'>
+                <h4>Crear / Editar</h4>
+                <button onClick={() => ejecutar()}>Ejecutar</button>
+                <button onClick={() => ejecutarBajar()}>Bajar 1/2 Tono</button>
+                <form onSubmit={handlerSubmit} id='formulario'>
+                    <div><button type='submit'>{edition ? 'Actualizar' : 'Crear'}</button> </div>
+                    <small>Título</small>
+                    <input type="text" id='titulo' />
+                    <small>Artista</small>
+                    <input type="text" id='artista' />
+                    <small>BPM</small>
+                    <input type="number" id='bpm' />
+                    <small>URL</small>
+                    <input type="text" id='url' />
+                    <small>Letra</small>
+                    <textarea id="letra" rows="20"></textarea>
+                    <small>Acordes</small>
+                    <textarea id="acordes" rows="20"></textarea>
+                </form>
             </div>
         </div>
     </>)
