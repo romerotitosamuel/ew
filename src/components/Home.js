aa9d7148da001931c3b022db42ed53e8e2b49fe4
tsamuel1289 <romerotitosamuel@gmail.com>
2022-02-03 16:55:52 -0400

    u

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,25 @@ const db = getFirestore(firebaseApp)
 const Home = () => {
 
     const [songsDom, setSongsDom] = useState([])
+    const [apps, setApps] = useState(false)
 
+
+    let iphone = null
+    let boss = 'https://play.google.com/store/apps/details?id=jp.co.roland.bosstuner&hl=es_BO&gl=US'
+    let soundBrenner = 'https://play.google.com/store/apps/details?id=com.soundbrenner.pulse&hl=es_BO&gl=US'
+    let bibleGateway = 'https://play.google.com/store/apps/details?id=com.csnmedia.android.bg&hl=es_BO&gl=US'
+    let nuestroPanDiario = 'https://play.google.com/store/apps/details?id=org.rbc.odb&hl=es_BO&gl=US'
+
+    if (navigator.userAgent.match(navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i))) {
+        iphone = true
+    } else { iphone = false }
+    if (iphone) {
+        boss = 'https://apps.apple.com/us/app/boss-tuner/id1113473319'
+        soundBrenner = 'https://apps.apple.com/us/app/the-metronome-by-soundbrenner/id1048954353'
+        bibleGateway = 'https://apps.apple.com/us/app/bible-gateway/id506512797'
+        nuestroPanDiario = 'https://apps.apple.com/pe/app/nuestro-pan-diario/id437496538'
+    }
+    
     const getSongsDom = async () => {
 
         if (window.navigator.onLine) {
@@ -24,7 +42,6 @@ const Home = () => {
                 paraLocal.push(songWId)
             })
         } else {
-            console.log("no hay internet")
             setSongsDom(JSON.parse(localStorage.getItem('localSongsDom')))
         }
     }
@@ -43,7 +60,7 @@ const Home = () => {
                 <img src={logo50} alt="No hay logo" />
             </div>
 
-            <div className="domHome">
+            <div className="domHome" style={{display: apps ? 'none' : 'block'}}>
                 {
                     songsDom.map((s) => {
                         return (
@@ -57,8 +74,31 @@ const Home = () => {
                     })
                 }
             </div>
-            <div className="aplications">
-
+            <div className="aplications" style={{display: apps ? 'block' : 'none'}}>
+                <div className="contApp">
+                    <img src="https://play-lh.googleusercontent.com/UsnCgUDSfqbFFjDGLgC3d8_n1CbOHe1JMjBmSeRw-pkJXGZ4v4DNQpYfMxA-x4ZH5a-6=s180-rw" alt="Boss Tuner" />
+                    <a href={boss} target="_blank">
+                        Boss Tuner (Afinador)
+                    </a  >
+                </div>
+                <div className="contApp">
+                    <img src="https://play-lh.googleusercontent.com/Ol4yyIJn3Or2v67kZrtVuvXDuBkwwhPidZReP07-kTjV38tc9IOHoWQ_WQo8cDMyKw=s180-rw" alt="Soundbrenner" />
+                    <a href={soundBrenner} target="_blank">
+                        Soundbrenner (metrónomo)
+                    </a  >
+                </div>
+                <div className="contApp">
+                    <img src="https://play-lh.googleusercontent.com/7HllI6WUdcDQ4ubbssOcsDmim_g-rSg2VrsMK7zrCUPjcsBRVm4u40PbW2cwV1uP6Zc=s180-rw" alt="Soundbrenner" />
+                    <a href={bibleGateway} target="_blank">
+                        Bible Gateway (Biblia)
+                    </a  >
+                </div>
+                <div className="contApp">
+                    <img src="https://play-lh.googleusercontent.com/5A3iedWtGCxhfSHpLkn1hfjz2ID5PEcOoPK8HKJasUp0OJAXpetbGPZiGkptbdOzWb0=s180-rw" alt="NuestroPanDiario" />
+                    <a href={nuestroPanDiario} target="_blank">
+                        Nuestro Pan Diario (Devocional)
+                    </a  >
+                </div>
             </div>
 
             <a className='youtubeArea' href="https://music.youtube.com/playlist?list=PL_FgA7_oWUXKWcagYkqu2DW9z7IC-BiOu" target="_blank">
@@ -67,13 +107,11 @@ const Home = () => {
             </a  >
 
             <div className="footerHome">
-                <Link to='/'><i className="material-icons" >home</i></Link>
-                <div ><i className="material-icons" >apps</i></div>
+                <Link to='/'><i className="material-icons" onClick={()=> setApps(false)} >home</i></Link>
+                <div ><i className="material-icons" onClick = {()=> setApps(true)}>apps</i></div>
                 <Link to='/lib'><i className="material-icons" >library_music</i></Link>
-
             </div>
         </div>
-
     </>)
 }
 export default Home
