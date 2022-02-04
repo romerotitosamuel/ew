ccd15747c8ead5317e57fcc823bfdd2625cf68d4
tsamuel1289 <romerotitosamuel@gmail.com>
2022-02-04 11:06:07 -0400

    Updates

---
+++ precache-manifest.83c79ec72e0495b5796cae3d22fdf35f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c2282f9569965b589a013a1601b995",
+    "url": "/ew/index.html"
+  },
+  {
+    "revision": "0f7496ad84e4e024e3a0",
+    "url": "/ew/static/css/main.0bc9c35a.chunk.css"
+  },
+  {
+    "revision": "b38bbbb9a5061be27006",
+    "url": "/ew/static/js/2.50c18549.chunk.js"
+  },
+  {
+    "revision": "983ee9998a3b01b0f3c879af78fa1933",
+    "url": "/ew/static/js/2.50c18549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e5705c9028c77a7acb5",
+    "url": "/ew/static/js/3.8ae02301.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/ew/static/js/3.8ae02301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b31e231bc716e71a61",
+    "url": "/ew/static/js/4.efbb7353.chunk.js"
+  },
+  {
+    "revision": "0f7496ad84e4e024e3a0",
+    "url": "/ew/static/js/main.2c5492bd.chunk.js"
+  },
+  {
+    "revision": "78372a5111308f46b1a2",
+    "url": "/ew/static/js/runtime-main.15831598.js"
+  },
+  {
+    "revision": "67054059a49965a8138c646d0c0cb630",
+    "url": "/ew/static/media/click1.67054059.mp3"
+  }
+]);
